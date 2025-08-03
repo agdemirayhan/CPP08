6a70a67ee3e8096e81f0d7c1f345c345aacc81ec
Ayhan Agdemir <aagdemir@2-H-1.42heilbronn.de>
2025-08-03 13:23:27 +0200

    ex00 better structure and some examples

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,37 +3,45 @@
 #include <list>
 #include "easyfind.hpp"
 
-int main() {
+template <typename T>
+void searchAndPrint(T& container, int valueToFind, const std::string& containerName) {
+    try {
+        typename T::iterator it = easyfind(container, valueToFind);
+        std::cout << "Found in " << containerName << ": " << *it << std::endl;
+    } catch (const std::exception &e) {
+        std::cerr << containerName << ": " << e.what() << " " << valueToFind << std::endl;
+    }
+}
+
+int main()
+{
     std::vector<int> vec;
     vec.push_back(10);
     vec.push_back(20);
     vec.push_back(30);
 
-    try {
-        std::vector<int>::iterator it = easyfind(vec, 20);
-        std::cout << "Found in vector: " << *it << std::endl;
-    } catch (const std::exception& e) {
-        std::cerr << "Vector: " << e.what() << std::endl;
-    }
-
-    try {
-        std::vector<int>::iterator it = easyfind(vec, 40);
-        std::cout << "Found in vector: " << *it << std::endl;
-    } catch (const std::exception& e) {
-        std::cerr << "Vector: " << e.what() << std::endl;
-    }
+    std::cout << "---- Vector Example ----" << std::endl;
+    std::cout << "Vector: ";
+    for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+    
+    searchAndPrint(vec, 20, "vector");
+    searchAndPrint(vec, 40, "vector");
 
     std::list<int> lst;
     lst.push_back(1);
     lst.push_back(2);
     lst.push_back(3);
+    
+    std::cout << "\n---- List Example ----" << std::endl;
+    std::cout << "List: ";
+    for (std::list<int>::iterator it = lst.begin(); it != lst.end(); ++it)
+    std::cout << *it << " ";
+    std::cout << std::endl;
 
-    try {
-        std::list<int>::iterator it = easyfind(lst, 3);
-        std::cout << "Found in list: " << *it << std::endl;
-    } catch (const std::exception& e) {
-        std::cerr << "List: " << e.what() << std::endl;
-    }
+    searchAndPrint(lst, 1, "list");
+    searchAndPrint(lst, 5, "list");
 
     return 0;
 }
